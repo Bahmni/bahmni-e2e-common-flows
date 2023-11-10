88e0d41cdfbd937e91a5ee6c9565f57c4420ae03
priyanshu1602 <priyanshu0549@gmail.com>
2023-11-11 01:26:00 +0530

    Fixed failing ,flaky test cases and refactoredCode

--- tests/reports.js
+++ tests/reports.js
@@ -1,12 +1,13 @@
 const { $, goto, below, write, textBox, into, click, toLeftOf, dropDown, checkBox, reload, text, timeField, waitFor, highlight, screenshot, toRightOf, button, switchTo, within } = require('taiko');
 var assert = require("assert")
 var date = require("./util/date");
+const { format } = require("date-fns")
 
 step("Select start date, end date and <reportFormat> format for <reportName> and click on run button", async function (reportFormat, reportName) {
-	let startDate = date.today()
-	let endDate = date.today()
-	await timeField(within($("//*[normalize-space()='" + reportName + "']/..")), below(text("Start Date"))).select(startDate)
-	await timeField(within($("//*[normalize-space()='" + reportName + "']/..")), toRightOf(text("Start Date")), below(text("End Date"))).select(endDate)
+	let startDate = format(date.today(), "dd/MM/yyyy")
+	let endDate = format(date.today(), "dd/MM/yyyy")
+	await write(startDate, into($("//TH[@class='reports-start-date']/input"), within($("//*[normalize-space()='" + reportName + "']/.."))))
+	await write(endDate, into($("//TH[@class='reports-stop-date']/input"), within($("//*[normalize-space()='" + reportName + "']/.."))))
 	await dropDown(within($("//*[normalize-space()='" + reportName + "']/..")), below(text("Format"))).select(reportFormat)
 	await click(button("Run Now"),toRightOf(text(reportName)), within($("//*[normalize-space()='" + reportName + "']/..")))
 });
